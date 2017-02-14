3754df0034d1192681fc0977902cfd5bb6d254f8
Sagetopia <developer@sagetopia.com>
2017-02-14 16:01:59 -0500

    Remove extraneous resources

--- assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-function updateViewportDimensions() {
-  var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
-  return { width:x,height:y };
-}
-
-(function($) {
-
-}(jQuery));
